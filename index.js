6c2612713f643dc22facf300cc1c133097c870f9
bonveeee <bonochieng@gmail.com>
2022-06-09 21:31:45 +0300

    commit tree c37e375cf8442321a4a6a04cb113802ef1bec13f

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solutions in this file
+const gifts = ["teddy bear","drone","doll"];
+
+function wrapGifts(gifts) {
+    let i = 0; 
+    while (i < gifts.length) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      i++; 
+    }
+  
+    return gifts;
+  }
+  
+
+for(let age=30;age<40;age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+
+wrapGift(gifts);
+
+function writeCards(stringArray,eventName){
+        const stringLength = stringArray.length;
+        const newArray = []
+    for(let i=0;i<stringLength;i++){
+        newArray.push(`Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`);
+        debugger
+    }
+    return newArray;
+}
+
+writeCards(["Guadalupe","Ollie","Aki"],"birthday");
+
+function countDown(countNum){
+    let i=countNum;
+    while(i>=0){
+        console.log(i);
+        i--;
+    }
+}
+
+countDown(10);
